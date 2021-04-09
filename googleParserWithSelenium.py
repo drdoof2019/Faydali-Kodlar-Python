2cd69fd023243841c8fd12a73c9c9db2e7557c91
drdoof2019 <50274757+drdoof2019@users.noreply.github.com>
2021-04-09 22:50:22 +0300

    Update googleParserWithSelenium.py

--- googleParserWithSelenium.py
+++ googleParserWithSelenium.py
@@ -7,9 +7,10 @@ driver = webdriver.Chrome()
 driver.get("https://www.google.com/search?q=hasan")
 #assert "Python" in driver.title
 time.sleep(1)
-for element in driver.find_elements_by_xpath('//div[@id="search"]//div[@class="g"]'):
+for element in driver.find_elements_by_xpath('//div[@id="search"]//div[@class="g"]'): # Burada id'si search olan divin içindeki class'ı g olan divi aradık
+    # Ana dizin yukardaki. Aşağıdakileri bu ana dizinin içinde aradığımız için . ile başladık.
     title = element.find_element_by_xpath('.//h3[@class="LC20lb DKV0Md"]').text
-    link = element.find_element_by_xpath('.//div[@class="yuRUbf"]/a').get_attribute('href')
+    link = element.find_element_by_xpath('.//div[@class="yuRUbf"]/a').get_attribute('href') # burada classı yurubf olan divin içindeki a daki href i aradık. fotosunu atıyorum.
     description = element.find_element_by_xpath('.//span[@class="aCOpRe"]').text
     #detail = element.find_element_by_xpath('.//span[@class="aCOpRe"]').text
     print(title)
